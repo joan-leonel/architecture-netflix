abbd6decb558cb581678dfdaeb0c5e5b06e5b5e3
joan leonel kenfack <Joan Leonel Kenfack>
2023-01-06 22:26:13 +0100

    comit projet

---
+++ frontWS/frontWS/src/test/java/com/example/frontWS/FrontWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.frontWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ webservice1/src/main/java/com/example/webservice1/Webservice1Application.java
@@ -0,0 +1,21 @@
+package com.example.webservice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Webservice1Application {
+
+	@GetMapping("/")
+	public String sayHello(){
+		return "Hello !";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webservice1Application.class, args);
+	}
+
+}
